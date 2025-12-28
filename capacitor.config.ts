f84db441d2dccf230cdd20274965c59bdfabfd76
duran984-dev <durang984@gmail.com>
2025-12-28 12:23:05 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.orgasolutions.utilityapp',
+  appName: 'AOUS-All in One Utility Suite',
+  webDir: 'build'
+};
+
+export default config;
